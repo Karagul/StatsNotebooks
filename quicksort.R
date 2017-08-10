da913e9b1a35139e5ca4f9ada347fd8cc7aecc28
szhang118 <szhang118@users.noreply.github.com>
2017-08-10 09:30:01 -0400

    Add files via upload

---
+++ quicksort.R
@@ -0,0 +1,10 @@
+quicksort<-function(x){
+    if(length(x)<=1) return(x)
+    pivot<-x[1]
+    therest<-x[-1]
+    sv1<-therest[therest<pivot]
+    sv2<-therest[therest>=pivot]
+    sv1<-quicksort(sv1)
+    sv2<-quicksort(sv2)
+    return(c(sv1,pivot,sv2))
+}
